80a2403658115ac4ac63ea009c08a33cf82ee953
Aliciar-Thornthwaite <aliciathornthwaite@gmail.com>
2018-05-03 11:43:00 +0100

    Canvas Rendered
    Allenemies array created
    player class created
    
    enemy and player objects instantiated

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
-
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
-
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -23,17 +22,31 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
+var Player = function (){
+this.sprite = 'images/char-princess-girl.png'
+};
+
 // This class requires an update(), render() and
+Player.prototype.update = function(dt) {
+
+};
+Player.prototype.render = function() {
+ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // a handleInput() method.
 
 
 // Now instantiate your objects.
+var enemy1 = new Enemy( 10,150);
+var enemy2 = new Enemy( 10, 250);
+var enemy3 = new Enemy( 10, 350);
+
 // Place all enemy objects in an array called allEnemies
-let allEnemies = []
+const allEnemies = [ enemy1, enemy2, enemy3 ]
 // Place the player object in a variable called player
-
-
+var player = new Player(175,150);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
